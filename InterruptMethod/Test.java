7f5a95bda80f096653772c8344569350948e6b20
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-09-30 23:53:03 +0530

    Add files via upload

---
+++ InterruptMethod/Test.java
@@ -0,0 +1,24 @@
+package InterruptMethod;
+
+public class Test extends Thread {
+    public void run() {
+    	try {
+    		
+			for(int i = 1 ; i <= 5; i++) {
+				
+				System.out.println(i);
+				Thread.sleep(1000);
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+    }
+    
+    
+    public static void main(String[] args) {
+		Test t = new Test();
+		t.start();
+		t.interrupt();
+		
+	}
+}
